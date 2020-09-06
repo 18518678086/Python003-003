ea9435c3110f42a71a4204a0db284b25663f2c1e
gmthal <mafuyuan@baijiahulian.com>
2020-09-06 17:53:02 +0800

    week03-1

--- week03/week03-1/zhexuejiawenti.py
+++ week03/week03-1/zhexuejiawenti.py
@@ -15,38 +15,40 @@ class  DiningPhilosophers:
         self.l=l
         self.eatn=eatn
         self.number=num  #0-4
-        self.leftn=num
-        self.rightn=num -1 if num >= 1 else dphnums -1
+        self.leftn=num   #0-4
+        self.rightn=num -1 if num >= 1 else dphnums -1  #3,2
 
     def __call__(self):
-        print(f'{self.number} start at {datetime.now().microsecond}')
+        print(f'{self.number} started at {datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}')
         while self.eatn > 0:
             with l[self.leftn]:
-                print(f'[{self.number},{self.leftn},2]')
+                #print(f'[{self.number},{self.leftn},2]')
                 if not l[self.rightn].locked():                       
                     with l[self.rightn]:
+                        print(f'{self.number} is eating at {datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}')
+                        print(f'[{self.number},{self.leftn},2]')
                         print(f'[{self.number},{self.rightn},2]')
-                        print(state)
                         sleep(1)
-                        q.put(self.number)
-            state[self.number]=0
-            self.eatn -= 1
+                        print(f'{self.number} is done at {datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}')
+                        self.eatn -= 1
+            #print(f'{self.number},{self.eatn}')
+            
+            
 
 
 if __name__ == '__main__':
     dphnums=5
-    q=queue.Queue(5)
+    #q=queue.Queue(5)
     #print(q.qsize())
     #state=[0]*5
+    #pool = ThreadPoolExecutor(5)
     l=[]
     for i in range(5):
         l.append(Lock())
-    #pool = ThreadPoolExecutor(5)
+
     t=[]
-    #print('init task')
     for i in range(5):
         t.append(threading.Thread(target=DiningPhilosophers(i,4,l)))
-    #print('start task')
     for i in range(5):
         t[i].start()
     for i in range(5):
